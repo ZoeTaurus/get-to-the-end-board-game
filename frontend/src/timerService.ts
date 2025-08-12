9e9fe84ee21d55bd10df86afe7c05584601dcd05
xin zan <xinzan@Mac.lan>
2025-08-12 20:38:43 +0100

    TIMER DEBUG: Add console logs to see if timer reaches 0 and events are dispatched

--- frontend/src/timerService.ts
+++ frontend/src/timerService.ts
@@ -7,6 +7,7 @@ class TimerService {
     this.intervalId = setInterval(() => {
       this.currentTime--; // Count down from 30
       if (this.currentTime === 0) { // Check if time = 0
+        console.log('Timer reached 0, ending game!'); // Debug log
         this.endGame(); // End game if it = 0
       }
     }, 1000);
@@ -25,6 +26,7 @@ class TimerService {
 
   private endGame() {
     // End the game when timer reaches 0
+    console.log('endGame called, dispatching gameTimeout event'); // Debug log
     this.stopTimer();
     // Emit a custom event that the React app can listen to
     window.dispatchEvent(new CustomEvent('gameTimeout'));
