8eeeb68a243b7d0dd788f6f33b21ef916e268f18
xin zan <xinzan@Mac.lan>
2025-08-13 10:48:29 +0100

    Replace TimerService with cleaner, more readable version

--- frontend/src/timerService.ts
+++ frontend/src/timerService.ts
@@ -1,48 +1,34 @@
 class TimerService {
   private timerId: NodeJS.Timeout | null = null;
   private onTimeout: (() => void) | null = null;
-  private timeLeft: number = 30; // Stored as a class property
+  private timeLeft: number = 30;
 
-  startTimer(onTimeout: () => void) {
+  private startCountdown(label: string) {
     this.stopTimer();
-    this.onTimeout = onTimeout;
-    this.timeLeft = 30; // Initialize when starting
-    
-    console.log('Timer started: 30 seconds');
-    
+    console.log(`${label}: 30 seconds`);
+
     this.timerId = setInterval(() => {
       this.timeLeft -= 1;
       console.log('Timer tick:', this.timeLeft);
-      
+
       if (this.timeLeft <= 0) {
         console.log('Timer reached 0! Ending game...');
         this.stopTimer();
-        if (this.onTimeout) {
-          this.onTimeout();
-        }
+        this.onTimeout?.();
       }
     }, 1000);
   }
 
-  resetTimer(onTimeout: () => void) {
-    this.stopTimer();
+  startTimer(onTimeout: () => void) {
     this.onTimeout = onTimeout;
-    this.timeLeft = 30; // Initialize when resetting
-    
-    console.log('Timer reset: 30 seconds');
-    
-    this.timerId = setInterval(() => {
-      this.timeLeft -= 1;
-      console.log('Timer tick:', this.timeLeft);
-      
-      if (this.timeLeft <= 0) {
-        console.log('Timer reached 0! Ending game...');
-        this.stopTimer();
-        if (this.onTimeout) {
-          this.onTimeout();
-        }
-      }
-    }, 1000);
+    this.timeLeft = 30;
+    this.startCountdown('Timer started');
+  }
+
+  resetTimer(onTimeout: () => void) {
+    this.onTimeout = onTimeout;
+    this.timeLeft = 30;
+    this.startCountdown('Timer reset');
   }
 
   stopTimer() {
